fc25cb3dea76b00b170ec51584c644ac0abb391b
Cem Aydin <cem.aydin@gmx.ch>
2019-05-21 22:12:44 +0200

    CSS: add z-index

--- src/reflect-app/user-dropdown-menu.js
+++ src/reflect-app/user-dropdown-menu.js
@@ -18,6 +18,7 @@ const style = html`
       position: relative;
       overflow: hidden;
       color: var(--light-text-hig-emph);
+      z-index: 1000;
     }
     .overlay {
       background-color: rgba(255, 255, 255, 0.16);
