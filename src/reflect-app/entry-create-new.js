add7424187e482ea319b8c2c0226fa80b582c2bf
Cem Aydin <cem.aydin@gmx.ch>
2019-11-10 04:04:32 +0100

    detection and validity logic (disable button, show hints etc.)

--- src/reflect-app/entry-create-new.js
+++ src/reflect-app/entry-create-new.js
@@ -40,6 +40,11 @@ const style = html`
       background-color: rgba(255, 255, 255, 0.10);
       padding: 10px;
     }
+    .hint {
+      margin
+      display: block;
+      color: var(--error);
+    }
   </style>
 `;
 
@@ -55,89 +60,72 @@ const event_created = new CustomEvent('created', {
 });
 
 class EntryCreateNew extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({mode: 'open'});
-
-    //this.detected_type = "";
-    //this.inputData = {};
-    this.inputHint = "";
-    this.update();
-  }
   get activeTopics() {
     return this._activeTopics || [];
   }
   set activeTopics(v) {
     this._activeTopics = v;
+    this.showHint = false;
+    this.update();
+  }
+  get entry() {
+    return this._entry || {};
+  }
+  set entry(v) {
+    this._entry = v;
+    this.showHint = false;
+    this.update();
+  }
+  get valid() {
+    if (this.entry.detection !== 'complete' || this.activeTopics.length < 1)
+      return false;
+    return true;
+  }
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.showHint = false;
+  }
+  connectedCallback() {
+    this.update();
   }
   async submit() {
+    if (!this.valid) {
+      this.showHint = true;
+      this.update();
+      return;
+    }
+    this.showHint = false;
+    this.update();
     console.log("submitting...");
     // check detection status
-    console.log("input data:", this.inputData);
-    if (!this.inputData) {
-      this.inputHint = "no input data found...";
-      this.update();
-      console.log("empty obj.");
-      return;
-    }
-    if (!this.inputData.ready) {
-      this.inputHint = this.inputData.detectedType.hint;
-      this.update();
-      console.log("not ready");
-      return;
-    }
-    /*const topics = this.shadowRoot.querySelector('main-menu').topics;
-    //console.log(topics);
-    if (!check_sel_active(topics)) {
-      this.input_err = "At least one topic must be selected...";
-      this.update()
-      return;
-    }
-    this.input_err = "";
-    this.update();
-    const params = {
-      det_type: this.detected_type.class,
-      text: this.textinput_el.value,
-      author: global_state.user.name,
-      sel_data: topics
-    };
-    // send request
-    const res = await api_req_post(entries_url, params, auth.get_auth_header());
-    if (res) {
-      this.reset_types();
-      this.textinput_el.value = "";
-      // -> make this flash msg later
-      this.input_err = "Entry created successfully! ID: " + res.id;
-      console.log(res);
-      this.dispatchEvent(event_created);
-      this.update();
-    } else {
-      this.input_err = "Error creating entry... :(";
-    }*/
-  }
-  detectUpdate(detail) {
-    this.inputData = detail;
 
-    this.update();
+  }
+  getHint() {
+    if (this.valid || this.entry.detection === 'initial') return html``;
+    if (this.entry.detection !== 'complete')
+      return html`<small class="hint">entry input incomplete...</small>`;
+    if (this.activeTopics.length < 1)
+      return html`<small class="hint">select one or more topics...</small>`;
   }
   update() {
     render(html`${style}
       <div>
         <entry-input class="inline"
-          @typedetected=${(e)=>this.detectUpdate(e.detail)}></entry-input>
-        <labelled-button class="inline"
+                     @change=${(e)=>this.entry = e.detail}></entry-input>
+        <labelled-button class="inline" ?disabledstyle=${!this.valid}
                          @click=${()=>this.submit()} label="Create"></labelled-button>
-       ${ this.inputHint != "" ?
-          html`<small id="input-err">${this.inputHint}</small>` :
-          html`` }
+        ${this.showHint ? html`${this.getHint()}` : html``}
       </div>
       <div id="input-overlay">
         <topics-list .activeTopics=${this.activeTopics}
-          @selectionchanged=${(e)=>{this.activeTopics=e.detail}}></topics-list>
+                     @selectionchanged=${(e)=>{this.activeTopics=e.detail}}></topics-list>
         <subtags-list></subtags-list>
       </div>
       `, this.shadowRoot);
   }
+  //?disabled=${this.entry.detection !== 'complete' ||
+  //this.activeTopics.length < 1}
 }
 
 customElements.define('entry-create-new', EntryCreateNew);
