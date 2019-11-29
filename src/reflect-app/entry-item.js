71c29731f7a0e3c41d25539cd1e70a431686d1cf
Cem Aydin <cem.aydin@gmx.ch>
2019-11-29 01:08:29 +0100

    rework entry-item

--- src/reflect-app/entry-item.js
+++ src/reflect-app/entry-item.js
@@ -1,5 +1,6 @@
 import { html, render } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import format from 'date-fns/format'
 const md = window.markdownit().use(window.markdownitEmoji);
 // add twemojis
 // (attribution req.)
@@ -14,8 +15,10 @@ const style = html`
     :host {
       display: block;
       box-sizing: border-box;
-      padding: 5px 15px 5px 15px;
+      padding: 5px 5px 5px 5px;
       color: var(--light-text-med-emph);
+      -background-color: #131e1e;
+      -border: 1px solid #164141;
     }
     :host(.private) {
       /*background-color: var(--bg-private);
@@ -28,6 +31,10 @@ const style = html`
       /*font-size: 0.8em;*/
       color: var(--light-text-low-emph);
     }
+    .le-header-text a {
+      /*font-size: 0.8em;*/
+      display: none;
+    }
     .le-header-icon {
       vertical-align: middle;
       padding-left: 3px;
@@ -38,7 +45,7 @@ const style = html`
       /*overflow: hidden;*/
       box-sizing: border-box;
       margin: 0 15px 5px 15px;
-      padding: 0 50px 5px 50px;
+      padding: 2px 50px 5px 50px;
       border-left: 1px solid var(--on-background-border);
       /*font-size: 18px;*/
       line-height: 1.5em;
@@ -57,15 +64,16 @@ class EntryItem extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
-
-    //this.get_entries();
+  }
+  connectedCallback() {
   }
   get entry() {
     return this._entry;
   }
-  set entry(entry) {
-    this._entry = entry;
-    this.state_update();
+  set entry(v) {
+    this._entry = v;
+    this.update();
+    //this.state_update();
   }
   state_update() {
     if (this.entry.private) {
@@ -73,13 +81,31 @@ class EntryItem extends HTMLElement {
     }
     this.update();
   }
+  getContent() {
+    if (this.entry.type === 'note') {
+      return html`<div class="le-body">${unsafeHTML(md.render(this.entry.text))}</div>`;
+    } else if (this.entry.type === 'link' || this.entry.type === 'brokenlink') {
+      return html`<div class="le-body">
+                    <p>${this.entry.title}</p>
+                    <a href=${this.entry.url}>${this.entry.url}</a><br />
+                    <small>${this.entry.info}</small>
+                    <div>${this.entry.comment !== "" ? this.entry.comment : html``}</div>
+                  </div>
+                 `;
+    }
+  }
   update() {
-    //<div >
-    //</div>
+    const dateFormat = "ddd MMM d YYYY HH:mm:ss Z"
+
     render(html`${style}
-      <small class="le-header-text">${this.entry.timestamp}
-        <a href="#entry?id=${this.entry.id}">#entry?id=${this.entry.id}</a>
-      </small>
+                <small class="le-header-text">
+                  ${format(this.entry.date, dateFormat)}
+                  <a href="#entry?id=${this.entry.id}">#entry?id=${this.entry.id}</a>
+                </small>
+                ${this.getContent()}
+      `, this.shadowRoot);
+      /*
+      <a href="#entry?id=${this.entry.id}">#entry?id=${this.entry.id}</a>
       ${ this.entry.private ?
         html`<img class="le-header-icon" src="layout/icons/private_32.png">
              <small class="le-header-text">(private)</small>` :
@@ -88,8 +114,7 @@ class EntryItem extends HTMLElement {
         html`<img class="le-header-icon" src="layout/icons/pin_16.png">
              <small class="le-header-text">(pinned)</small>` :
         html`` }
-      <div class="le-body">${unsafeHTML(md.render(this.entry.content.text))}</div>
-      `, this.shadowRoot);
+      */
   }
 }
 
